8d2384a6a63c0c04dbeb26ca93d8303d7aa998e8
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-06 15:34:02 +0200

    create basket system

--- wish-vue/assets/js/vue.js
+++ wish-vue/assets/js/vue.js
@@ -23,28 +23,45 @@ const home = {
             products,
             searchKey: '',
             liked: [],
+            cart: [],
         }
     },
     computed: {
         filteredList(){
             return this.products.filter((product) => {
-                return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
+              return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
             })
         },
 
         getLikeCookie(){
-            let cookieValue = JSON.parse($cookies.get('like'));
-            cookieValue == null ? this.liked = [] : this.liked = cookieValue;
-        }
+        let cookieValue = JSON.parse($cookies.get('like'));
+        cookieValue == null ? this.liked = [] : this.liked = cookieValue
+        },
     },
     methods: {
         setLikeCookie(){
             document.addEventListener('input', () => {
-             setTimeout(() => {
-                $cookies.set('like', JSON.stringify(this.liked));
+                setTimeout(() => {
+                    $cookies.set('like', JSON.stringify(this.liked));
                 }, 300);
             })
-        }
+        },
+
+        addToCart(product){
+            // check if already in array
+            for (let i = 0; i < this.cart.length; i++){
+              if (this.cart[i].id === product.id) {
+                return this.cart[i].quantity++
+              }
+            }
+            this.cart.push({
+              id: product.id,
+              img: product.img,
+              description: product.description,
+              price: product.price,
+              quantity: 1
+            })
+          },
     },
     mounted: () => {
         this.getLikeCookie;
