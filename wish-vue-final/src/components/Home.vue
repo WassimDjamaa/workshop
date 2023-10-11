7bc5f6ccd9225c237bd910976ce85f0b947ccee2
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-11 11:06:36 +0200

    fix bug display image

--- wish-vue-final/src/components/Home.vue
+++ wish-vue-final/src/components/Home.vue
@@ -13,7 +13,7 @@
           <div class="card" v-for="product in filteredList" :key="product.id">
             <div class="img-container">
               <!-- Utilisation de l'URL relative pour charger l'image -->
-              <img :src="`@/assets/img/${product.img}`" :alt="product.description" />
+              <img :src="`${product.img}`" alt="product">
             </div>
             <div class="product-details">
               <h2>{{ product.description }}</h2>
@@ -31,7 +31,18 @@
     data() {
       return {
         products: [
-          { id: 1, description: 'Quarz Luxe', price: 12, img: 'quarz-luxe.JPG' },
+          { id: 1, description: 'Quarz Luxe', price: 12, img: '../src/assets/img/quarz-luxe.JPG' },
+          { id: 2, description: 'Curren Business', price: 20, img: '../src/assets/img/curren-business.JPG' },
+          { id: 3, description: 'Curren Sport', price: 5, img: '../src/assets/img/curren-sport.JPG' },
+          { id: 4, description: 'Jaragar Racing', price: 8, img: '../src/assets/img/jaragar-racing.JPG' },
+          { id: 5, description: 'Liges Hommes', price: 3, img: '../src/assets/img/liges-hommes.JPG' },
+          { id: 6, description: 'Maserati Mechanical', price: 65, img: '../src/assets/img/maserati-mechanical.JPG' },
+          { id: 7, description: 'Montre Mecanique', price: 25, img: '../src/assets/img/montre-mecanique.JPG' },
+          { id: 8, description: 'Brand Designer', price: 28, img: '../src/assets/img/brand-designer.JPG' },
+          { id: 9, description: 'Relogio Masculino', price: 4, img: '../src/assets/img/relogio-masculino.JPG' },
+          { id: 10, description: 'Tissot Multifunction', price: 29, img: '../src/assets/img/tissot-multifunction.JPG' },
+          { id: 11, description: 'Hip Hop Gold', price: 87, img: '../src/assets/img/hiphop-gold.JPG' },
+          { id: 12, description: 'Mesh Genova', price: 6, img: '../src/assets/img/mesh-genova.JPG' },
         ],
         searchKey: '',
       };
