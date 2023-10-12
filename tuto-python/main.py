7ef49bd87f90a3cf4723acee0ff985598b88ff03
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-12 16:31:10 +0200

    init project python

---
+++ tuto-python/main.py
@@ -0,0 +1,6 @@
+lastname = input("Quelle est ton nom ? ")
+firstname = input("Quelle est ton prenom ? ")
+
+name_complet = firstname + " " + lastname 
+
+print(f"Bonjour je m'appelle {name_complet} et je code pour la 1ere fois en python")
