226f00ab9cac8d03e7540418e5ba26c92f724b20
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-19 09:15:25 +0200

    reformat class Person

--- tuto-python/initiation/Person.py
+++ tuto-python/initiation/Person.py
@@ -5,10 +5,39 @@ class Person:
         self.last_name = last_name
         self.ag = ag
 
-prenom = input("Entrer votre prenom : ")
-nom = input("Entrer votre nom : ")
-age = input("Entrer votre age : ")
+    def __check__(self):
+        while True:
+            self.first_name = input("Entrer votre prénom: ")
+            if not self.first_name:
+                print("Vous n'avez pas entré votre prénom.")
+            else:
+                break
 
-personneA = Person(prenom, nom, age)
+        while True:
+            self.last_name = input("Entrer votre nom: ")
+            if not self.last_name:
+                print("Vous n'avez pas entré votre nom.")
+            else:
+                break
 
-print(f"Je m'appelle {personneA.first_name} {personneA.last_name} et j'ai {personneA.ag} ans")
+        while True:
+            self.ag = input("Entrer votre âge: ")
+            if not self.ag:
+                print("Vous n'avez pas entré votre âge.")
+            elif not self.ag.isdigit():
+                print("L'âge doit être un nombre.")
+            else:
+                break
+
+ 
+    def __str__(self):
+        return f"Je m'appelle {self.first_name} {self.last_name} et j'ai {self.ag} ans"
+    
+
+if __name__ == "__main__":
+
+    person = Person("","","")
+
+    person.__check__()
+
+    print(person)
