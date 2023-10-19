d9821fe0a5dcb328ee02045387b19a1bd8a1a044
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-19 17:02:38 +0200

    reformat ES6 code

--- wish-vue-final/server.js
+++ wish-vue-final/server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import { json, urlencoded } from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //routes
 app.post('/register', (req, res) => {
