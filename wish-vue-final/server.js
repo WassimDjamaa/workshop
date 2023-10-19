bf0076d7ad330d14ac35836f65a322062341b5cd
WassimDjamaa <wassim.djamaa@sfr.fr>
2023-10-19 15:08:32 +0200

    create script js for the backend

---
+++ wish-vue-final/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    console.log(`Server running on port ${port}`)
+});
